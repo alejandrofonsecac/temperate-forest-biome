93fb7d4fd79ec7a4935f5b4f5c06e80dc184bee5
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-06-08 13:47:11 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,14 +3,19 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/frontend/', // nome do repositório
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
     port: 3000,
+
     // Proxy para o backend Spring Boot durante desenvolvimento
     proxy: {
       '/api': {
